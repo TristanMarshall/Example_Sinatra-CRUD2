7ee91fc033d3fac458e565e16cb9948fd25a63b9
TristanMarshall <tristanmarshall@ymail.com>
2015-07-28 15:41:02 -0500

    message

---
+++ controllers/HomeController.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+
+#get localhost
+  get '/' do
+
+    erb :index
+
+  end
+
+end
--- controllers/StudentsController.rb
+++ controllers/StudentsController.rb
@@ -3,9 +3,9 @@ class StudentsController < ApplicationController
   get '/' do
 
 
-    #get ALL of the students!
-    students = Students.all.to_json
-
+    #get ALL of the students from
+    @students = Students.all
+    erb :students_index 
   end
 
 end
